36431bc987b385ac889adfe4037ec38e461c3228
narikub <kiranahumaira86@gmail.com>
2022-05-23 20:37:59 +0700

    initial commit

--- routes/bayi.js
+++ routes/bayi.js
@@ -1,8 +1,7 @@
-const express = require('express')
-const router = express.Router()
-const Bayi = require('../models/Bayi')
-
-const verifyToken = require('../routes/verifyToken')
+const express       = require('express')
+const router        = express.Router()
+const Bayi          = require('../models/Bayi')
+const verifyToken   = require('../routes/verifyToken')
 
 //create
 router.post('/', async (req, res) => {
