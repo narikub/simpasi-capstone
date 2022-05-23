4c696eaa9335e6e1e5e4afb698da55a5fe2a06b0
narikub <kiranahumaira86@gmail.com>
2022-05-23 20:27:14 +0700

    update router

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require('dotenv/config')
-
-//file storage multer
+require('dotenv').config()
+const express       = require('express')
+const app           = express()
+const bodyParser    = require('body-parser')
+const mongoose      = require('mongoose')
+const cors          = require('cors')
 
 //Middleware
 app.use(bodyParser())
@@ -14,15 +12,20 @@ app.use(cors())
 //import routes
 const bayiRoutes = require('./routes/bayi')
 const userRoutes = require('./routes/auth')
+const passwordReset = require('./routes/passwordReset')
+
+app.use(express.json());
 
 //routes
-app.use('/bayi', bayiRoutes);
+app.use('/bayi', bayiRoutes)
 app.use('/user', userRoutes)
+app.use('/forgot', passwordReset)
 
 //connect to db
-mongoose.connect(process.env.DB_CONNECTION)
+mongoose.connect(process.env.DB_CONNECTION, {
+    useUnifiedTopology: true,
+})
 let db = mongoose.connection
-
 db.on('error', console.error.bind(console, 'Database connect Error!'))
 db.once('open', () => {
     console.log('Database is Connected')
