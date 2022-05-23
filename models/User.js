57a168728866f0f0afb4319c279e42a7fe3d68ed
narikub <kiranahumaira86@gmail.com>
2022-05-23 20:33:01 +0700

    initial commit

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose  = require('mongoose')
+const Schema    = mongoose.Schema
 
-const userSchema = mongoose.Schema({
+const userSchema = new Schema({
     nama: {
         type: String,
         required: true,
