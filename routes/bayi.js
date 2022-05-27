34413828de4ff47eb7af057738ce154c65e459dc
narikub <kiranahumaira86@gmail.com>
2022-05-28 04:23:19 +0700

    relationship bayi - user

--- routes/bayi.js
+++ routes/bayi.js
@@ -1,10 +1,11 @@
 const express       = require('express')
-const router        = express.Router()
+const router        = new express.Router()
 const Bayi          = require('../models/Bayi')
+const User          = require('../models/User')
 const verifyToken   = require('../routes/verifyToken')
 
 //create
-router.post('/', async (req, res) => {
+router.post('/', verifyToken, async (req, res) => {
     const bayiPost = new Bayi({
         nama: req.body.nama,
         tglLahir: req.body.tglLahir,
@@ -12,7 +13,8 @@ router.post('/', async (req, res) => {
         jk_bayi: req.body.jk_bayi,
         tb_bayi: req.body.tb_bayi,
         bb_bayi: req.body.bb_bayi,
-        alergi: req.body.alergi
+        alergi: req.body.alergi,
+        user: req.user._id
     })
     
     try {
@@ -26,10 +28,12 @@ router.post('/', async (req, res) => {
 //read all bayi
 router.get('/', verifyToken, async (req, res) => {
     try {
-        const bayi = await Bayi.find()
-        res.json(bayi)
-    } catch (err) {
-        res.json({message: err})
+        const bayi = await Bayi.find({user:req.user._id})
+        if (!bayi)
+        return res.status(404).send()    
+        res.status(200).send(bayi)
+    }catch(e) {
+        res.status(400).send(e.message)
     }
 })
 
@@ -44,7 +48,7 @@ router.get('/:id_bayi', verifyToken, async (req, res) => {
 })
 
 //update
-router.put('/:id_bayi', async (req, res) => {
+router.put('/:id_bayi', verifyToken, async (req, res) => {
     try {
         const bayiUpdate = await Bayi.updateOne({ _id: req.params.id_bayi}, {
             nama: req.body.nama,
@@ -62,7 +66,7 @@ router.put('/:id_bayi', async (req, res) => {
 })
 
 //delete
-router.delete('/:id_bayi', async (req, res) => {
+router.delete('/:id_bayi', verifyToken, async (req, res) => {
     try {
         const bayiUpdate = await Bayi.deleteOne({ _id: req.params.id_bayi})
         res.json(bayiUpdate)
