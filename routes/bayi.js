8a6c048b6397dbf272d7bb7e48c1dfa31fe0c203
narikub <kiranahumaira86@gmail.com>
2022-06-06 17:31:01 +0700

    update action cek bahan

--- routes/bayi.js
+++ routes/bayi.js
@@ -1,27 +1,33 @@
 const express       = require('express')
-const router        = new express.Router()
+const router        = express.Router()
 const Bayi          = require('../models/Bayi')
-const User          = require('../models/User')
 const verifyToken   = require('../routes/verifyToken')
+
+const { bayiValidation, cekBahan, editBayiValidation }  = require('../configs/validation')
 
 //create
 router.post('/', verifyToken, async (req, res) => {
+    //validasi
+    const { error } = bayiValidation(req.body)
+    if(error) return res.status(400).json({
+        status: res.statusCode,
+        message: error.details[0].message
+    })
     const bayiPost = new Bayi({
         nama: req.body.nama,
         tglLahir: req.body.tglLahir,
-        umur: req.body.umur,
         jk_bayi: req.body.jk_bayi,
-        tb_bayi: req.body.tb_bayi,
         bb_bayi: req.body.bb_bayi,
-        alergi: req.body.alergi,
         user: req.user._id
     })
-    
     try {
         const bayi = await bayiPost.save()
         res.json(bayi)
     } catch (err) {
-        res.json({message: err})
+        res.status(400).json({
+            status: res.statusCode,
+            message: 'Gagal menambahkan data bayi'
+        })
     }
 })
 
@@ -32,8 +38,8 @@ router.get('/', verifyToken, async (req, res) => {
         if (!bayi)
         return res.status(404).send()    
         res.status(200).send(bayi)
-    }catch(e) {
-        res.status(400).send(e.message)
+    }catch(err) {
+        res.status(400).send(err.message)
     }
 })
 
@@ -42,26 +48,52 @@ router.get('/:id_bayi', verifyToken, async (req, res) => {
     try {
         const bayi = await Bayi.findOne({ _id: req.params.id_bayi})
         res.json(bayi)
-    } catch (err) {
-        res.json({message: err})
+    } catch(err) {
+        res.status(400).send(err.message)
     }
 })
 
 //update
 router.put('/:id_bayi', verifyToken, async (req, res) => {
+    const { error } = editBayiValidation(req.body)
+    if(error) return res.status(400).json({
+        status: res.statusCode,
+        message: error.details[0].message
+    })
     try {
         const bayiUpdate = await Bayi.updateOne({ _id: req.params.id_bayi}, {
             nama: req.body.nama,
             tglLahir: req.body.tglLahir,
-            umur: req.body.umur,
             jk_bayi: req.body.jk_bayi,
-            tb_bayi: req.body.tb_bayi,
             bb_bayi: req.body.bb_bayi,
+        })
+        res.json(bayiUpdate)
+    } catch (err) {
+        res.status(400).json({
+            status: res.statusCode,
+            message: 'Gagal mengedit data bayi'
+        })
+    }
+})
+
+//update cek bahan bayi
+router.put('/:id_bayi/bahan', verifyToken, async (req, res) => {
+    //validasi
+    const { error } = cekBahan(req.body)
+    if(error) return res.status(400).json({
+        status: res.statusCode,
+        message: error.details[0].message
+    })
+    try {
+        const bayiUpdate = await Bayi.updateOne({ _id: req.params.id_bayi}, {
             alergi: req.body.alergi
         })
         res.json(bayiUpdate)
     } catch (err) {
-        res.json({message: err})
+        res.status(400).json({
+            status: res.statusCode,
+            message: 'Gagal mengedit data bayi'
+        })
     }
 })
 
@@ -70,8 +102,8 @@ router.delete('/:id_bayi', verifyToken, async (req, res) => {
     try {
         const bayiUpdate = await Bayi.deleteOne({ _id: req.params.id_bayi})
         res.json(bayiUpdate)
-    } catch (err) {
-        res.json({message: err})
+    } catch(err) {
+        res.status(400).send(err.message)
     }
 })
 
