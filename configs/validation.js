10ce9c36a5b83d0debadf2f4fe1db48e2a53920e
narikub <kiranahumaira86@gmail.com>
2022-06-06 17:22:52 +0700

    add cek bahan validation

--- configs/validation.js
+++ configs/validation.js
@@ -35,10 +35,6 @@ const loginValidation = (data) => {
 //Validasi Change password
 const changePasswordValidation = (data) => {
     const schema = Joi.object({
-        //old_password: Joi.string()
-        //    .min(6)
-        //    .required()
-        //    .valid(Joi.ref('loginValidation/password')),
         password: Joi.string()
             .min(6)
             .required(),
@@ -49,6 +45,67 @@ const changePasswordValidation = (data) => {
     return schema.validate(data)
 }
 
+const bayiValidation = (data) => {
+    const schema = Joi.object({
+        nama: Joi.string()
+            .required(),
+        tglLahir: Joi.date()
+            .required(),
+        jk_bayi: Joi.string()
+            .required()
+            .enum = ["P", "L"],
+        bb_bayi: Joi.string()
+            .required(),
+    })
+    return schema.validate(data)
+}
+
+const editBayiValidation = (data) => {
+    const schema = Joi.object({
+        nama: Joi.string(),
+        tglLahir: Joi.date(),
+        jk_bayi: Joi.string()
+            .enum = ['P', 'L'],
+        bb_bayi: Joi.string()
+    })
+    return schema.validate(data)
+}
+
+const cekBahan = (data) => {
+    const schema = Joi.object({
+        alergi: Joi.object({
+            Roti_Tawar: Joi.string()
+                .required()
+                .enum = ['1', '2', '3', '4'],
+            Cumi_Cumi: Joi.string()
+                .required()
+                .enum = ['1', '2', '3', '4'],
+            Tepung_Beras: Joi.string()
+                .required()
+                .enum = ['1', '2', '3', '4'],
+            Pisang: Joi.string()
+                .required()
+                .enum = ['1', '2', '3', '4'],
+            Telur_Bebek: Joi.string()
+                //.required()
+                .enum = ['1', '2', '3', '4'],
+            Kacang_Tanah: Joi.string()
+                //.required()
+                .enum = ['1', '2', '3', '4'],
+            Kerang: Joi.string()
+                //.required()
+                .enum = ['1', '2', '3', '4'],
+            Alpukat: Joi.string()
+                //.required()
+                .enum = ['1', '2', '3', '4'],
+        }).required()
+    })
+    return schema.validate(data)
+}
+
 module.exports.registerValidation       = registerValidation
 module.exports.loginValidation          = loginValidation
 module.exports.changePasswordValidation = changePasswordValidation
+module.exports.bayiValidation           = bayiValidation
+module.exports.editBayiValidation       = editBayiValidation
+module.exports.cekBahan                 = cekBahan
